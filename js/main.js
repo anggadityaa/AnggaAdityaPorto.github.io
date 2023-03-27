cc30b623067d8c44b52807ce038303961d30eca3
Angga Aditya <90344608+anggadityaa@users.noreply.github.com>
2023-03-27 22:23:39 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Ubah Bentuk Image di konten 1
+function ubahBentuk1() {
+  img1.style.width = "350px";
+  img1.style.margin = "-25px";
+  img1.style.paddingBottom = "20px";
+  img1.style.transition = "0.5s";
+}
+function ubahBentuk11() {
+  img1.style.width = "300px";
+  img1.style.margin = "0";
+  img1.style.paddingBottom = "0";
+}
+function ubahBentuk2() {
+  img2.style.width = "350px";
+  img2.style.margin = "-25px";
+  img2.style.paddingBottom = "20px";
+  img2.style.transition = "0.5s";
+}
+function ubahBentuk22() {
+  img2.style.width = "300px";
+  img2.style.margin = "0";
+  img2.style.paddingBottom = "0";
+}
+function ubahBentuk3() {
+  img3.style.width = "350px";
+  img3.style.margin = "-25px";
+  img3.style.paddingBottom = "20px";
+  img3.style.transition = "0.5s";
+}
+function ubahBentuk33() {
+  img3.style.width = "300px";
+  img3.style.margin = "0";
+  img3.style.paddingBottom = "0";
+}
